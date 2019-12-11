f2ef279fe1f5155d01e7c6be916470544d1488b2
Noppadol Lanngain <noppadol@igeargeek.com>
2019-12-11 23:19:19 +0700

    [Added] chat message

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,60 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-socket
-      </h1>
-      <h2 class="subtitle">
-        My excellent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div class="message" v-for="(msg, index) in messages" :key="index">
+      {{ msg }}
+    </div>
+    <div class="form">
+      <input autocomplete="off" v-model="message" />
+      <button class="btn" type="button" @click="sendMessage">Send</button>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import socket from '../plugins/socket.io'
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      message: '',
+      messages: [],
+    }
+  },
+  mounted() {
+    socket.on('sendMessage', (msg) => {
+      this.messages.push(msg)
+    })
+  },
+  methods: {
+    sendMessage() {
+      socket.emit('sendMessage', this.message)
+      this.message = ''
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style scoped>
+  .form { 
+    background: #000; 
+    padding: 3px; 
+    position: fixed; 
+    bottom: 0; 
+    width: 100%; 
+  }
+  input { 
+    border: 0; 
+    padding: 10px; 
+    width: 90%; 
+    margin-right: .5%; 
+  }
+  .btn { 
+    width: 9%; 
+    background: rgb(130, 224, 255); 
+    border: none; 
+    padding: 10px; 
+  }
+  .message {
+    margin-left: 50px;
+  }
 </style>
---
+++ socket.io.js
@@ -0,0 +1,21 @@
+const Express = require('express')
+const app = new Express()
+
+const http = require('http').Server(app)
+
+const io = require('socket.io')(http)
+
+const port = 3001
+
+io.on('connection', function (socket) {
+  // connected io success
+  console.log('connected : ID =>', socket.id)
+
+  socket.on('sendMessage', (msg) => {
+    io.emit('sendMessage', msg)
+  })
+})
+
+http.listen(port, function () {
+  console.log('Socket run on port:', port)
+})
